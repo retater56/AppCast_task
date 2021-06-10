e6bccb41ecd67d33ffe40abdff46768da7fa97ff
retater56 <70779585+retater56@users.noreply.github.com>
2021-06-10 22:11:08 +0300

    init: start develop with webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: 'style.css'
+      })
+  ]
+};
